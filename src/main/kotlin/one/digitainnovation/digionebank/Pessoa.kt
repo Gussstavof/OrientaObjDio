49f9045eb5c3873bb65f4f6960473497dc883991
unknown <g717397@gmail.com>
2021-03-10 11:54:45 -0300

    commit inicial

---
+++ src/main/kotlin/one/digitainnovation/digionebank/Pessoa.kt
@@ -0,0 +1,14 @@
+package one.digitainnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Gustavo"
+    var cpf: String = "000.000.000-00"
+
+}
+
+fun main(){
+    val Gustavo = Pessoa()
+
+    println(Gustavo.nome)
+    println(Gustavo.cpf)
+}
