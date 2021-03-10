c7fedce08a3aeea8afa21d09d51c12da549bb95e
unknown <g717397@gmail.com>
2021-03-10 12:33:28 -0300

    second commit

--- src/main/kotlin/one/digitainnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitainnovation/digionebank/Pessoa.kt
@@ -2,13 +2,24 @@ package one.digitainnovation.digionebank
 
 class Pessoa {
     var nome: String = "Gustavo"
+
     var cpf: String = "000.000.000-00"
+    private set
+
+
+    /*inner class Endereco{
+        var rua: String = "Rua teste"
+    }*/
 
 }
 
 fun main(){
     val Gustavo = Pessoa()
 
+
     println(Gustavo.nome)
     println(Gustavo.cpf)
+    println(Gustavo)
+
+    //println(Gustavo.Endereco().rua)
 }
